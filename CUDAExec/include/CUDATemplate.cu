1cd63e4178fdb0006f51d7483f9af2d5dd67876d
Ahmed Mahmoud <ahmed.mahmoud@autodesk.com>
2023-04-21 16:58:35 -0400

    Update CUDATemplate.cu

--- CUDAExec/include/CUDATemplate.cu
+++ CUDAExec/include/CUDATemplate.cu
@@ -7,24 +7,35 @@
 
 #include <cuda_profiler_api.h>
 
+#define nthreads 32
+
+struct myFloat4
+{
+    float x, y, z, w;
+};
+
 __global__ void test6(float* ptr, float* out)
 {
-    
-    __shared__ float4 shareM[32];
-    float4*           ptr_temp = (float4*)ptr;
-    for (int i = 0; i < 10; i++) {
-        if (threadIdx.x < 32) {
-            shareM[threadIdx.x] = ptr_temp[threadIdx.x];
-        }
-        __syncwarp();
+
+    __shared__ myFloat4 shareM[nthreads];
+    myFloat4*           ptr_temp = (myFloat4*)ptr;
+    // for (int i = 0; i < 10; i++) {
+    if (threadIdx.x < nthreads) {
+        ((float*)(shareM))[threadIdx.x] = ((float*)(ptr_temp))[threadIdx.x];
+        //printf("\n Read T=%d => %p", threadIdx.x, &shareM[threadIdx.x]);
     }
+    __syncwarp();
+    //}
 
     out[threadIdx.x] = ((float*)(shareM))[threadIdx.x];
+    //printf(
+    //    "\n Write T=%d => %p", threadIdx.x, &((float*)(shareM))[threadIdx.x]);
 }
 
 int main(int argc, char** argv)
 {
-    size_t bytes = sizeof(float4) * 32;
+
+    size_t bytes = sizeof(myFloat4) * nthreads;
     float *ptr, *out;
     CUDA_ERROR(cudaMalloc((void**)&ptr, bytes));
     CUDA_ERROR(cudaMalloc((void**)&out, bytes));
@@ -34,7 +45,7 @@ int main(int argc, char** argv)
 
     CUDA_ERROR(cudaProfilerStart());
 
-    test6<<<1, 32>>>(ptr, out);
+    test6<<<1, nthreads>>>(ptr, out);
 
     CUDA_ERROR(cudaDeviceSynchronize());
 
